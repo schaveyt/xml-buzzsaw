5290a1c9ff0ba5baa512c8dda1106d2c847820a9
Todd Schavey <todd.schavey@ge.com>
2017-03-13 01:11:48 -0400

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("xml-buzzaaw (c) 2017\n");
+        }
+    }
+}
